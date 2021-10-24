04a7efc8a1a955bfa83545e48a0164d3f236c5c4
nico <nickthenico@gmail.com>
2021-10-24 15:06:47 -0700

    Supposed to be a fix for a pull function error in ffmpeg

--- cogs/music.py
+++ cogs/music.py
@@ -18,7 +18,8 @@ ytdl_format_options = {
     'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 ffmpeg_options = {
-    'options': '-vn'
+    'options': '-vn',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
