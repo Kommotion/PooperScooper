03e737f41de9c11c55ffc175400e1b6f438f0d80
nico <nickthenico@gmail.com>
2021-11-16 08:44:47 -0800

    Adding ability to opt in and opt out of grammar police and get error counts of other users

--- cogs/grammarpolice.py
+++ cogs/grammarpolice.py
@@ -3,9 +3,10 @@ import json
 import language_tool_python
 from discord.ext import commands
 from discord.ext.commands import Cog
+from typing import Union
 
 
-EMRON_ID = 786822389610577921
+EMRON_ID = "786822389610577921"
 YOUR = "your"
 YOURE = "you're"
 YOUR_NN = 'YOUR_NN'
@@ -35,24 +36,42 @@ class GrammarErrors:
         finally:
             self.dump_json()
 
+    def get_error_count(self, user_id):
+        try:
+            return self.grammar_errors[user_id]
+        except KeyError:
+            return None
+
+    def add_member(self, user_id):
+        self.grammar_errors[user_id] = 0
+        self.dump_json()
+
+    def remove_member(self, user_id):
+        try:
+            del self.grammar_errors[user_id]
+            self.dump_json()
+            return True
+        except KeyError:
+            return False
+
 
 class GrammarPolice(Cog):
-    """Commands for monitor grammar mistakes. """
+    """Commands for monitoring grammar mistakes. """
 
     def __init__(self, bot):
         self.bot = bot
         self.spell_checker = language_tool_python.LanguageToolPublicAPI('en-US')
-        self.grammar_errors = GrammarErrors()
+        self.grammar = GrammarErrors()
 
     @commands.Cog.listener('on_message')
     async def emron_checker(self, message):
-        if message.author.id == EMRON_ID:
+        if str(message.author.id) in self.grammar.grammar_errors:
             content = message.content.lower()
             if YOUR in content or YOURE in content:
                 matches = self.spell_checker.check(content)
                 for match in matches:
                     if match.ruleId in [YOUR_NN, YOUR_UPPERCASE]:
-                        self.grammar_errors.increment_error(message.author.id)
+                        self.grammar.increment_error(str(message.author.id))
                         await self.send_error_message(match, message)
 
     async def send_error_message(self, match, message):
@@ -65,14 +84,18 @@ class GrammarPolice(Cog):
         await message.reply(embed=embed)
 
     @commands.command()
-    async def emron_errors(self, ctx):
-        """Prints out the number of grammar errors Emron has made. """
+    async def errors(self, ctx, *, user: Union[discord.Member, discord.User] = None):
         async with ctx.typing():
-            try:
-                errors = self.grammar_errors.grammar_errors[EMRON_ID]
-                description = "Emron's Your vs You're Error Count: {}".format(errors)
-            except KeyError:
-                description = "Emron hasn't made an error yet!"
+            member = user if user else ctx.message.author
+            member_id = str(member.id)
+            errors = self.grammar.get_error_count(member_id)
+
+            if errors is None:
+                description = "{} has not opted into error checking!".format(member.mention)
+            elif errors == 0:
+                description = "{} has not made any errors!".format(member.mention)
+            else:
+                description = "{} Your vs You're Error Count: {}".format(member.mention, errors)
 
             embed = discord.Embed(
                 title=GRAMMAR_POLICE,
@@ -80,6 +103,26 @@ class GrammarPolice(Cog):
                 colour=discord.Colour.blue()
             )
             await ctx.send(embed=embed)
+
+    @commands.command()
+    async def opt_in(self, ctx):
+        """Opts out of Grammar Police monitoring. """
+        async with ctx.typing():
+            member_id = str(ctx.message.author.id)
+            if member_id in self.grammar.grammar_errors:
+                await ctx.message.send('You are already opted in!')
+                return
+
+            self.grammar.add_member(member_id)
+            await ctx.message.add_reaction('👍')
+
+    @commands.command()
+    async def opt_out(self, ctx):
+        """Opts out of Grammar Police monitoring. WARNING: Deletes all of your grammar mistake data! """
+        async with ctx.typing():
+            member_id = str(ctx.message.author.id)
+            self.grammar.remove_member(member_id)
+            await ctx.message.add_reaction('👍')
 
 
 def setup(bot):
